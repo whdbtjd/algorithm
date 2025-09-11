fdfe590a810c86a872e9fe41bb4da41af384159a
Jo Yu Sung <cys990617@gmail.com>
2025-09-11 22:37:52 +0900

    2884

---
+++ 2884.py
@@ -0,0 +1,15 @@
+h,m = map(int,input().split())
+
+total = 60 * h + m
+
+if h == 0 and m < 45:
+    h = 23
+    m += 15
+else:
+    total -= 45
+    h = total // 60
+    m = total % 60
+
+print(f"{h} {m}")
+
+
