8bffa76e73537616a4ba2a3985d1441194b3f791
Jo Yu Sung <cys990617@gmail.com>
2025-10-24 14:39:42 +0900

    네 번째 점

---
+++ 3009.py
@@ -0,0 +1,37 @@
+ary = []
+
+x_ans = 0
+x_cnt = 0
+
+y_ans = 0
+y_cnt = 0
+
+ans = 0
+
+for i in range(3):
+    x,y = map(int,input().split())
+    ary.append((x,y))
+
+if ary[0][0] == ary[1][0]:
+    x_ans = ary[2][0]
+elif ary[0][0] == ary[2][0]:
+    x_ans = ary[1][0]
+else:
+    x_ans = ary[0][0]
+
+if ary[0][1] == ary[1][1]:
+    y_ans = ary[2][1]
+elif ary[0][1] == ary[2][1]:
+    y_ans = ary[1][1]
+else:
+    y_ans = ary[0][1]
+
+print(x_ans,y_ans)
+
+
+
+
+
+
+
+
