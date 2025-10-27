f39bd87353aa41163b0386a1af733555beb13c17
Jo Yu Sung <cys990617@gmail.com>
2025-10-27 22:02:02 +0900

    블랙잭

---
+++ 2798.py
@@ -0,0 +1,24 @@
+n,m = map(int,input().split())
+ary = []
+total_sum = set()
+
+ary = list(map(int,input().split()))
+
+for i in range(n-2):
+    for j in range(i+1,n-1):
+        for k in range(j+1,n):
+            num = ary[i] + ary[j] + ary[k]
+            total_sum.add(num)
+
+total_sum = list(total_sum)
+
+if m in total_sum:
+    print(m)
+else:
+    total_sum.append(m)
+    total_sum.sort()
+    print(total_sum[total_sum.index(m)-1])
+
+
+
+
