fb991e956d21aafa1ca9b3e9cfaeba144cd8046c
Yu Seong <cys990617@gmail.com>
2025-12-17 12:05:26 +0900

    하노이탑

---
+++ 11729.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+def hanoi(n, start, mid, end):
+    if n == 1:
+        print(start, end)
+        return
+    hanoi(n-1, start, end, mid)
+    print(start, end)
+    hanoi(n-1, mid, start, end)
+
+print(2**n - 1)
+hanoi(n, 1, 2, 3)
