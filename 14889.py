159f7224e2141af48db808226b20c9046dd37bab
Yu Seong <cys990617@gmail.com>
2025-12-31 18:20:08 +0900

    스타트와 링크

--- 14889.py
+++ 14889.py
@@ -1,41 +1,35 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
-s = [list(map(int, input().split())) for _ in range(n)]
+ary = [list(map(int, input().split())) for _ in range(n)]
 
-visited = [False] * n
-ans = 10**9
+check = [False] * n
+ans = []
 
 def dfs(idx, cnt):
-    global ans
-
     if cnt == n // 2:
         start = []
         link = []
 
         for i in range(n):
-            if visited[i]:
+            if check[i]:
                 start.append(i)
             else:
                 link.append(i)
 
-        a = b = 0
-        for i in range(n // 2):
-            for j in range(i + 1, n // 2):
-                a += s[start[i]][start[j]] + s[start[j]][start[i]]
-                b += s[link[i]][link[j]] + s[link[j]][link[i]]
+        start_sum = 0
+        link_sum = 0
 
-        ans = min(ans, abs(a - b))
+        for i in range(len(start)):
+            for j in range(i + 1, len(start)):
+                start_sum += ary[start[i]][start[j]] + ary[start[j]][start[i]]
+                link_sum += ary[link[i]][link[j]] + ary[link[j]][link[i]]
+
+        ans.append(abs(start_sum - link_sum))
         return
 
     for i in range(idx, n):
-        if not visited[i]:
-            visited[i] = True
-            dfs(i + 1, cnt + 1)
-            visited[i] = False
+        check[i] = True
+        dfs(i + 1, cnt + 1)
+        check[i] = False
 
-visited[0] = True
-dfs(1, 1)
-
-print(ans)
+dfs(0, 0)
+print(min(ans))
